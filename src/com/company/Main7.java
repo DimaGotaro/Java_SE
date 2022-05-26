5136afd64dfab81c8613f742ac0e3c17e4e107c8
DimaGotaro <gotaros@yandex.by>
2022-05-27 00:00:49 +0300

    39

--- src/com/company/Main7.java
+++ src/com/company/Main7.java
@@ -1,5 +1,11 @@
 package com.company;
 
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main7 {
     public static void main(String[] args) {
         // Многопоточное программирование
@@ -44,7 +50,6 @@ public class Main7 {
         catch (InterruptedException ex) {
             System.out.println(ex.getMessage());
         }
-        M_Thread gr4 = new M_Thread();
 
         /*Runnable grR3 = () -> {
             System.out.println(Thread.currentThread().getName() + " started...");
@@ -135,6 +140,118 @@ public class Main7 {
             System.out.println(ex.getMessage());
         }
         System.out.println();
+//        S_Thread gr4 = new S_Thread("S2_Thread");
+//        gr4.start(); // без join открывается и закрывается после "Главный поток закрыт..."
+
+        // Синхронизация потоков. Оператор synchronized
+        ComRes dt = new ComRes();
+        for (int i = 1; i < 4; i++) {
+            Thread dt2 = new Thread(new Com_Thread(dt));
+            dt2.setName("Com_Thread " + i);
+            dt2.start();
+            try {
+                dt2.join();
+            }
+            catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        System.out.println();
+
+        // можно синхронизировать метод
+        ComRes2 dt3 = new ComRes2();
+        for (int i = 1; i < 4; i++) {
+            Thread dt4 = new Thread(new Com_Thread2(dt3), "Com_Thread2 " + i);
+            dt4.start();
+            try {
+                dt4.join();
+            }
+            catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        System.out.println();
+
+        // Методы wait и notify
+        // задача с магазином, wait насильно прерывает synchronized
+        Magazin dt5 = new Magazin();
+        Pokyp dt6 = new Pokyp(dt5);
+        Zavod dt7 = new Zavod(dt5);
+        Thread dt8 = new Thread(dt7); // Zavod
+        Thread dt9 = new Thread(dt6); // Pokyp
+        dt9.start(); // Pokyp
+        dt8.start(); // Zavod
+        try {
+            dt8.join();
+            dt9.join();
+        }
+        catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println();
+
+        // Семафоры
+//        Semaphore sem = new Semaphore(2);
+//        Peremen per = new Peremen();
+//        new Thread(new Isp_Sem(per, sem, "Isp_Sem 1")).start();
+//        new Thread(new Isp_Sem(per, sem, "Isp_Sem 2")).start();
+//        new Thread(new Isp_Sem(per, sem, "Isp_Sem 3")).start();
+//        System.out.println();
+
+        // Философы
+//        Semaphore sem2 = new Semaphore(2); // за столом максимум 2 философа
+//        for (int i = 0; i < 6; i++) { // 5 философов всего
+//            Thread dt11 = new Thread(new Filosof(sem2, "Философ " + i, 2, i)); // t - приёмы пищи
+//            dt11.start();
+//        }
+//        System.out.println();
+
+        // Обмен между потоками. Класс Exchanger
+        // передача сообщения между двумя классами
+        Exchanger<String> cx = new Exchanger<String>();
+        new Thread(new PutEx(cx)).start();
+        new Thread(new GetEx(cx)).start();
+//        new Thread(new GetEx2(cx)).start(); // только два класса!
+
+        // Класс Phaser
+        Phaser ph = new Phaser(1);
+        new Thread(new K_Phaser(ph, "K_Phaser 1")).start();
+        new Thread(new K_Phaser(ph, "K_Phaser 2")).start();
+        // заканчивается фаза 0
+        int phase = ph.getPhase();
+        ph.arriveAndAwaitAdvance(); // начинаем фазу 1
+        System.out.println("Фаза " + phase + " завершена");
+        // заканчивается фаза 1
+        phase = ph.getPhase();
+        ph.arriveAndAwaitAdvance(); // начинаем фазу 2
+        System.out.println("Фаза " + phase + " завершена");
+        // заканчивается фаза 2
+        phase = ph.getPhase();
+        ph.arriveAndAwaitAdvance(); // начинаем фазу 3
+        System.out.println("Фаза " + phase + " завершена");
+        ph.arriveAndDeregister(); // отмена регистраций всех участников
+
+        // Блокировки. ReentrantLock
+        Chil cx2 = new Chil();
+        ReentrantLock cx3 = new ReentrantLock();
+        for (int i = 0; i < 4; i++) {
+            Thread cx4 = new Thread(new Look_Turead(cx2, cx3), "Look_Turead " + i);
+            cx4.start();
+        }
+
+        // Условия в блокировках
+        Magazin2 cx7 = new Magazin2();
+        Thread cx5 = new Thread(new Pokyp2(cx7));
+        Thread cx6 = new Thread(new Zavod2(cx7));
+        cx5.start();
+        cx6.start();
+        try {
+            cx5.join();
+            cx6.join();
+        }
+        catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
 
         System.out.println();
         System.out.println("Главный поток закрыт...");
@@ -200,7 +317,7 @@ class A_Thread extends Thread {
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + " started...");
+        System.out.println(getName() + " started...");
         int count = 1;
         while (!isInterrupted()) { // условие - поток не может быть остановлен, потому что он итак остановлен
             System.out.println("Loop" + count++);
@@ -216,7 +333,7 @@ class A_Thread extends Thread {
                 break;
             }
         }
-        System.out.println(Thread.currentThread().getName() + " finished...");
+        System.out.println(getName() + " finished...");
     }
 }
 class A2_Thread implements Runnable { // без Thread.sleep, как выше
@@ -237,7 +354,7 @@ class A3_Thread extends Thread { // 3 способ, если цикл оберн
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName() + " started...");
+        System.out.println(getName() + " started...");
         int count = 1;
         try {
             while (!isInterrupted()) {
@@ -248,6 +365,385 @@ class A3_Thread extends Thread { // 3 способ, если цикл оберн
         catch (InterruptedException ex) {
             System.out.println(getName() + " has been interrupted");
         }
+        System.out.println(getName() + " finished...");
+    }
+}
+// синхронизация переменной
+class ComRes {
+    int x = 0;
+}
+class Com_Thread implements Runnable {
+    ComRes res;
+    Com_Thread(ComRes res) {
+        this.res = res;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " started...");
+        synchronized (res) { // без метода res некорректно работает
+            res.x = 1;
+            for (int i = 1; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + " Loop " + res.x);
+                res.x++;
+                try {
+                    Thread.sleep(100);
+                }
+                catch (InterruptedException ex) {
+                    System.out.println(Thread.currentThread().getName() + " has been interrupted");
+                }
+            }
+        }
         System.out.println(Thread.currentThread().getName() + " finished...");
+    }
+}
+// можно синхронизировать метод
+class ComRes2 {
+    int x = 0;
+
+    synchronized void incr() { // можно синхронизировать метод
+        System.out.println(Thread.currentThread().getName() + " started...");
+        x = 1;
+        for (int i = 1; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " Loop " + x);
+            x++;
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException ex) {
+                System.out.println(Thread.currentThread().getName() + " has been interrupted");
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + " finished...");
+    }
+}
+class Com_Thread2 implements Runnable {
+    ComRes2 res;
+    Com_Thread2(ComRes2 res) {
+        this.res = res;
+    }
+
+    @Override
+    public void run() {
+        res.incr(); // ссылка на синхронизированный метод
+    }
+}
+// Методы wait и notify
+// задачка с магазином
+class Magazin {
+    int prodyct = 0;
+    synchronized void get() {
+        while (prodyct < 1) {
+            try {
+                wait();
+            }
+            catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        prodyct--;
+        System.out.println("Покупатель забрал 1 товар");
+        System.out.println("На складе осталось товара: " + prodyct);
+        notify();
+    }
+    synchronized void put() {
+        while (prodyct >= 3) {
+            try {
+                wait();
+            }
+            catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        prodyct++;
+        System.out.println("Производитель добавил 1 товар");
+        System.out.println("На складе осталось товара: " + prodyct);
+        notify();
+    }
+}
+class Pokyp implements Runnable {
+    Magazin mag;
+    Pokyp(Magazin mag) {
+        this.mag = mag;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            mag.get();
+        }
+    }
+}
+class Zavod implements Runnable {
+    Magazin mag;
+    Zavod(Magazin mag) {
+        this.mag = mag;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            mag.put();
+        }
+    }
+}
+// Семафоры
+class Peremen {
+    int x = 0;
+}
+class Isp_Sem implements Runnable {
+    Peremen per;
+    Semaphore sem;
+    String name;
+    Isp_Sem(Peremen per, Semaphore sem, String name) {
+        this.per = per;
+        this.sem = sem;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(name + " ожидает подключения");
+            sem.acquire();
+            per.x = 1;
+            for (int i = 1; i < 5; i++) {
+                System.out.println(this.name + ": " + per.x);
+                per.x++;
+                Thread.sleep(100);
+            }
+        }
+        catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(name + " освобождает разрешение");
+        sem.release();
+    }
+}
+class Filosof implements Runnable {
+    String name;
+    Semaphore sem;
+    int t; // кол-во приёмов пищи
+    int id; // i в цикле потоков, кол-во философов
+
+    Filosof(Semaphore sem, String name, int t, int id) {
+        this.sem = sem;
+        this.name = name;
+        this.t = t;
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= t; i++) {
+                sem.acquire();
+                System.out.println(name + " сел жрать");
+                if (id%2 == 0) { // чётный философ
+                    Thread.sleep(150); // философ жрёт
+                }
+                else { // нечётный философ
+                    Thread.sleep(200); // философ жрёт
+                }
+                System.out.println(name + " закончил жрать");
+                sem.release();
+                Thread.sleep(400); // философ гуляет
+            }
+        }
+        catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+// Обмен между потоками. Класс Exchanger
+class PutEx implements Runnable {
+    Exchanger<String> ex;
+    String mes;
+    PutEx(Exchanger<String> ex) {
+        this.ex = ex;
+        mes = "Hello priblyda";
+    }
+
+    @Override
+    public void run() {
+        try {
+            mes = ex.exchange(mes);
+            System.out.println("Сообщение отправлено - " + mes);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+class GetEx implements Runnable {
+    Exchanger<String> ex;
+    String mes;
+    GetEx(Exchanger<String> ex) {
+        this.ex = ex;
+        mes = "Hello лох";
+    }
+
+    @Override
+    public void run() {
+        try {
+            mes = ex.exchange(mes);
+            System.out.println("Сообщение получено - " + mes);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+class GetEx2 implements Runnable {
+    Exchanger<String> ex;
+    String mes;
+    GetEx2(Exchanger<String> ex) {
+        this.ex = ex;
+        mes = "Hello fanboy";
+    }
+
+    @Override
+    public void run() {
+        try {
+            mes = ex.exchange(mes);
+            System.out.println("Сообщение получено - " + mes);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+// Класс Phaser
+class K_Phaser implements Runnable {
+    Phaser ph;
+    String name;
+    K_Phaser(Phaser ph, String name) {
+        this.ph = ph;
+        this.name = name;
+        ph.register();
+    }
+    @Override
+    public void run() {
+        System.out.println(name + " выполняет фазу " + ph.getPhase());
+        ph.arriveAndAwaitAdvance();
+        try {
+            Thread.sleep(200);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(name + " выполняет фазу " + ph.getPhase());
+        ph.arriveAndAwaitAdvance();
+        try {
+            Thread.sleep(200);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(name + " выполняет фазу " + ph.getPhase());
+        ph.arriveAndDeregister();
+    }
+}
+// Блокировки. ReentrantLock
+class Chil {
+    int x = 0;
+}
+class Look_Turead implements Runnable {
+    Chil res;
+    ReentrantLock loock;
+    Look_Turead(Chil res, ReentrantLock loock) {
+        this.res = res;
+        this.loock = loock;
+    }
+
+    @Override
+    public void run() {
+        loock.lock();
+        try {
+            res.x = 1;
+            for (int i = 1; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + " Loop " + res.x);
+                res.x++;
+                Thread.sleep(100);
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            loock.unlock();
+        }
+    }
+}
+// Условия в блокировках
+class Magazin2 {
+    int prod = 0;
+    ReentrantLock ret;
+    Condition con;
+    Magazin2() {
+        ret = new ReentrantLock();
+        con = ret.newCondition();
+    }
+    public void get() {
+        ret.lock();
+        try {
+            while (prod < 1) {
+                con.await();
+            }
+            prod--;
+            System.out.println("Покупатель забрал 1 товар");
+            System.out.println("На складе осталось товара: " + prod);
+            con.signalAll();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            ret.unlock();
+        }
+    }
+    public void put() {
+        ret.lock();
+        try {
+            while (prod >= 3) {
+                con.await();
+            }
+            prod++;
+            System.out.println("Производитель добавил 1 товар");
+            System.out.println("На складе осталось товара: " + prod);
+            con.signalAll();
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        finally {
+            ret.unlock();
+        }
+    }
+}
+class Pokyp2 implements Runnable {
+    Magazin2 mag;
+    Pokyp2(Magazin2 mag) {
+        this.mag = mag;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            mag.get();
+        }
+    }
+}
+class Zavod2 implements Runnable {
+    Magazin2 mag;
+    Zavod2(Magazin2 mag) {
+        this.mag = mag;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            mag.put();
+        }
     }
 }
