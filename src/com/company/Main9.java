9ae6b6b64b5381fafb9099ab485653756d5bfb4b
DimaGotaro <gotaros@yandex.by>
2022-06-01 19:34:00 +0300

    44

--- src/com/company/Main9.java
+++ src/com/company/Main9.java
@@ -1,8 +1,29 @@
 package com.company;
 
+import com.sun.jdi.Value;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 public class Main9 {
     public static void main(String[] args) {
-        // Модульность
-        // Создание модуля
+        // Дополнительные классы
+        // Математические вычисления и класс Math
+        System.out.println(Math.abs(-5)); // абсолютное значение(модуль)
+        System.out.println(Math.pow(5, 2)); // 5 в квадрате
+        System.out.println(Math.PI*Math.pow(5, 2)); // площадь круга с радиусом 5
+
+        // Большие числа BigInteger и BigDecimal
+        BigInteger a = BigInteger.valueOf(568975156);
+        BigInteger b = BigInteger.valueOf(389329875);
+        BigInteger c = a.multiply(b);
+        System.out.println(c);
+        long x = c.longValue();
+        System.out.println(x);
+
+        BigDecimal a1 = BigDecimal.valueOf(56851698412.518);
+        BigDecimal a2 = BigDecimal.valueOf(38745216888.589);
+        BigDecimal a3 = a1.multiply(a2);
+        System.out.println(a3);
     }
 }
